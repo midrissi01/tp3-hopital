4c8ac6cab04191bc7ae5e1dd26faa423d6495d7b
midrissi01 <m.oulhaj_idrissi@mundiapolis.ma>
2023-11-09 16:01:00 +0100

    first commit

---
+++ src/main/java/ma/mundiapolis/tp3hopital/Tp3HopitalApplication.java
@@ -0,0 +1,40 @@
+package ma.mundiapolis.tp3hopital;
+
+import ma.mundiapolis.tp3hopital.entities.Patient;
+import ma.mundiapolis.tp3hopital.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class Tp3HopitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3HopitalApplication.class, args);
+
+        }
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository ) {
+        return args -> {
+            patientRepository.save(
+                    new Patient(null,"Hassan",new Date(),false,12));
+            patientRepository.save(
+                    new Patient(null,"Mohammed",new Date(),true,14));
+            patientRepository.save(
+                    new Patient(null,"Hanaa",new Date(),false,2));
+            patientRepository.save(
+                    new Patient(null,"Yasmine",new Date(),true,53));
+
+
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+
+        };
+    }
+}
+
+
---
+++ src/test/java/ma/mundiapolis/tp3hopital/Tp3HopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mundiapolis.tp3hopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3HopitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
